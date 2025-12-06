74365e930d7c7ffd920d4b13af27d41fc744cfe3
arham213 <76661263+arham213@users.noreply.github.com>
2025-12-06 16:51:56 +0500

    fix:bug-fix-in-server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv';
 import connectDB from './src/config/db.js';
 import app from './src/app.js';
-import startCronJobs from './src/utils/scheduler.js';
+// import startCronJobs from './src/utils/scheduler.js';
 
 dotenv.config();
 
 const startServer = async () => {
     await connectDB();
-    startCronJobs();
+    // startCronJobs();
     const PORT = process.env.PORT || 8000;
     app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
 }
