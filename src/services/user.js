954542b911624dd64eba2a8a2cb4ff3f22a4b356
arham213 <76661263+arham213@users.noreply.github.com>
2026-02-18 07:53:25 +0500

    fix:added-logs

--- src/services/user.js
+++ src/services/user.js
@@ -353,10 +353,12 @@ const generateOTPUpdateUserAndSendEmail = async (user, isSimpleOTP) => {
 
   try {
     console.log('sending email');
-    // await sendEmail(user.email, 'Email Verification', `Please verify your email\nYour OTP is: ${code}`);
+    await sendEmail(user.email, 'Email Verification', `Please verify your email\nYour OTP is: ${code}`);
     console.log('email sent');
   } catch (error) {
-    throw new AppError("Something went wrong. Please try again later.", 500);
+    // throw new AppError("Something went wrong. Please try again later.", 500);
+    console.error("EMAIL ERROR:", error);
+    throw error;
   }
 
   if (isSimpleOTP) {
--- src/utils/emailServer.js
+++ src/utils/emailServer.js
@@ -1,16 +1,27 @@
 import nodemailer from "nodemailer";
 
-const getTransporter = () => {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: "arhamasjid213@gmail.com",
-            pass: process.env.GOOGLE_APP_PASSWORD
-        }
-    })
+// const getTransporter = () => {
+//     const transporter = nodemailer.createTransport({
+//         service: "gmail",
+//         auth: {
+//             user: "arhamasjid213@gmail.com",
+//             pass: process.env.GOOGLE_APP_PASSWORD
+//         }
+//     })
 
-    return transporter;
-}
+//     return transporter;
+// }
+
+const getTransporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.GOOGLE_APP_PASSWORD
+    }
+});
+
 
 export const sendEmail = async (to, subject, message) => {
     const transporter = getTransporter();
